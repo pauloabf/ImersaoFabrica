50d62b55796763658ae13c96fb32e276b95c35aa
pauloabf <113388177+pauloabf@users.noreply.github.com>
2022-09-13 08:40:40 -0300

    Add files via upload

---
+++ chamar uma funcao.py
@@ -0,0 +1,3 @@
+from exercicio1 import multiplicacao
+
+print(multiplicacao)
---
+++ exercicio1.py
@@ -0,0 +1,9 @@
+def multiplicacao(num_1,num_2):
+    return num_1 * num_2
+
+numero_1 = int(input("Me fale um primeiro numero para a multiplicacao:"))
+numero_2 = int(input("Me fale um segundo numero para multiplicacao:"))
+
+multiplicacao_numeros = multiplicacao(numero_1,numero_2)
+
+print(multiplicacao_numeros)
